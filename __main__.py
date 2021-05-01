1acc080ea655c95e8c9b7c0ec326dc75be9a9d67
Matteo Capasso <mcapass2@binghamton.edu>
2021-05-01 15:27:57 -0700

    Initial repo setup

---
+++ __main__.py
@@ -0,0 +1,11 @@
+import os, sys, time, logging, psutil
+from util import *
+
+#psutil Docs: https://psutil.readthedocs.io/en/latest/
+#Logging Docs: https://docs.python.org/3/library/logging.html
+
+def main(): 
+    pass
+
+if __name__ == "__main__":
+    pass
